7f91426adfc394a855abcc7be853cfa195be27de
kmzer06 <keithmpofu06@icloud.com>
2025-04-30 06:28:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Program.cs (using top-level statements)
+using DungeonExplorer;
+using System.Linq;
+
+// Setup map and player
+var map = new GameMap();
+var room1 = new Room(1, "You are in a dimly lit cave.");
+var room2 = new Room(2, "You enter a narrow corridor.");
+var room3 = new Room(3, "You've found a large chamber!");
+
+room1.Monsters.Add(new Goblin());
+room1.Items.Add(new Weapon("Rusty Sword", 3));
+
+room2.Monsters.Add(new Dragon());
+room2.Items.Add(new Potion("Small Health Potion", 20));
+
+room3.Items.Add(new Weapon("Steel Sword", 10));
+room3.Items.Add(new Potion("Large Health Potion", 50));
+
+map.AddRoom(room1);
+map.AddRoom(room2);
+map.AddRoom(room3);
+
+map.ConnectRooms(1, "north", 2);
+map.ConnectRooms(2, "south", 1);
+map.ConnectRooms(2, "east", 3);
+map.ConnectRooms(3, "west", 2);
+
+var player = new Player("Hero");
+
+// Start game
+new Game(player, map).Start();
+Console.WriteLine("Game Over! Thanks for playing.");
+Console.ReadKey();
+
+// Note: The above code assumes the existence of classes like Player, GameMap, Room, Monster, etc.
+// and their respective methods and properties as defined in the original code snippets.
---
+++ ReadMe.txt
@@ -0,0 +1,34 @@
+Dungeon Explorer
+
+Table of Contents
+
+1. [Features] 
+2. [Item System] 
+3. [Inventory]
+4. [Prerequisites]  
+
+------------------
+------------------
+
+--- [Features] ----
+- Creature Hierarchy 
+  -- Creature --  
+     - 'Player' and multiple 'Monster' subclasses (such as'Goblin', 'Dragon')  
+
+
+
+--- [Item System] ---
+  - 'Item' with the 'Weapon' and 'Potion'  
+  - `ICollectible` interface for pickup behavior  
+
+--- [Inventory Management] ---
+
+- GameMap & Navigation
+  - 'GameMap' manages 'Room' instances  
+  - Commands: 'go <direction>', 'look', 'inventory', 'attack' , `use <item>`  
+
+
+[Prerequisites]
+- [.NET 7.0 SDK] (or later)  
+- [C# Dev Kit for VS Code] 
+
